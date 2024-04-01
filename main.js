c59c5b9b7efd4e89d27f375ba45b7b3e8f87ee32
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-01 22:51:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+//function boardResult()// {
+//    return 351 / 550 * 100//
+//}
+//let result =boardResult()
+//console.log(result);
+function obtainedMarks() {
+    return "englishObtainmarks =74\n,urduObtainmarks =56\n,mathsObtainmarks =65\n,chemistryObtainedmarks =59\n,physicsObtainmarks =48\n,computerObtainmarks =66\n,islamiatObtainmarks =69\n,totalObtainedmarks =437\n";
+}
+function totalMarks() {
+    return "englishTotalmarks =75\n,urduTotalmarks =75\n,mathsTotalmarks =75\n,chemistryTotalmarks =75\n,physicsTotalmarks =75\n,computerTotalmarks =75\n,islamiatTotalmarks =75\n,totalMarks=550\n";
+}
+console.log(obtainedMarks());
+console.log(totalMarks());
+let totalObtainedmarks = 437;
+let totalmarks = 550;
+function percentage() {
+    return totalObtainedmarks / totalmarks * 100;
+}
+console.log(percentage());
